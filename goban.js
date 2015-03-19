9bcd9ddac0e24277c8b85194262568523f6acc82
Takatoshi Kondo <redboltz@gmail.com>
2015-03-19 22:28:27 +0900

    Implemented the alternative move. Commented out websocket connection working codes. Added game guide message.

--- goban.js
+++ goban.js
@@ -17,6 +17,9 @@ Position.prototype = {
     clone: function() {
         return new Position(this.x, this.y)
     },
+    equals: function(other) {
+        return this.x === other.x && this.y === other.y
+    }
 }
 Position.distance = function(a, b) {
     return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y)
@@ -42,6 +45,7 @@ Score.prototype = {
         this.turn = 0
         this.MAXTurn = 0
         this.history = {}
+        this.move = BLACK
         this.board = {}
         for (x = 0; x < MAX; ++x) {
             this.board[x] = {}
@@ -52,7 +56,15 @@ Score.prototype = {
     },
     put: function(pos) {
         this.history[this.turn] = pos
-        this.putStone(pos, this.turn % 2 ? WHITE : BLACK)
+        this.putStone(pos, this.move)
+        if (this.move == BLACK) this.move = WHITE
+        else this.move = BLACK
+        ++this.turn
+    },
+    pass: function() {
+        this.history[this.turn] = new Position(-1, -1)
+        if (this.move == BLACK) this.move = WHITE
+        else this.move = BLACK
         ++this.turn
     },
     getStone: function(pos) {
@@ -119,13 +131,18 @@ $(window).on('load', function() {
     const CANVAS_WIDTH  = BASE * (MAX + 1)
     const CANVAS_HEIGHT = BASE * (MAX + 1)
 
-    var score = new Score()
+    var score = {}
     var goban      = document.getElementById('goban')
     var stone      = document.getElementById('stone')
     var number     = document.getElementById('number')
     var focus      = document.getElementById('focus')
     var yaku       = document.getElementById('yaku')
+    var guide      = document.getElementById('guide')
     var input      = document.getElementById('input')
+    var start      = document.getElementById('start')
+    var pass       = document.getElementById('pass')
+    var resign     = document.getElementById('resign')
+    var altMove    = document.getElementById('alt_move')
     var showNumber = document.getElementById('show_number')
     var showYaku   = document.getElementById('show_yaku')
     var gameType   = document.getElementById('gametype')
@@ -134,6 +151,7 @@ $(window).on('load', function() {
     var ctxNumber = number.getContext('2d')
     var ctxFocus  = focus.getContext('2d')
     var ctxYaku   = yaku.getContext('2d')
+    var ctxGuide  = guide.getContext('2d')
     var scoreView = document.getElementById("scoreView")
 
     const COM   = {}
@@ -141,15 +159,21 @@ $(window).on('load', function() {
     var blackPlayer = HUMAN
     var whitePlayer = HUMAN
 
-
+    var playing = false
+    var openingMove = 0
     var yakuPositions
+    var alt5first = null
+    var alt5second = null
     drawBoard(goban.getContext('2d'))
+
+    $(pass).prop('disabled', true)
+    $(resign).prop('disabled', true)
 
     // Checkboxes
     $(showNumber).on('change', function(e) {
         ctxNumber.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-        if (showNumber.checked) 
-            for (i = 0; i < score.turn; ++i) 
+        if (showNumber.checked)
+            for (i = 0; i < score.turn; ++i)
                 putNumber(ctxNumber, score.getPosition(i), i + 1)
     })
     $(showYaku).on('change', function(e) {
@@ -173,21 +197,111 @@ $(window).on('load', function() {
             whitePlayer = HUMAN
         }
     })
-    
+
+
+    // Start click
+    $(start).on('click', function() {
+        playing = true
+        $(resign).prop('disabled', false)
+        $(gameType).prop('disabled', true)
+        $(altMove).prop('disabled', true)
+        $(pass).prop('disabled', true)
+        score = new Score()
+        $(scoreView).empty()
+        ctxStone.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+        ctxYaku.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+        ctxFocus.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+        ctxNumber.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+        if (blackPlayer == HUMAN) {
+            var pos = new Position(7, 7)
+            score.put(pos)
+            putStone(ctxStone, pos)
+            if (showNumber.checked) putNumber(ctxNumber, pos, score.turn)
+            if (altMove.checked) {
+                alt5first = null
+                alt5second = null
+            }
+            var scoreText = sprintf("%3d: %s: %s", 1, "Black", pos.toString())
+            var $pre = $('<pre>').text(scoreText)
+            var $newLi = $('<li>').append($pre).appendTo(scoreView)
+            moveFocus($newLi)
+            $(scoreView).parent().scrollTop($(scoreView)[0].scrollHeight)
+            drawGuide(ctxGuide)
+        }
+    })
+
+    // Resign click
+    $(resign).on('click', function() {
+        playing = false
+        $(resign).prop('disabled', true)
+        $(gameType).prop('disabled', false)
+        $(altMove).prop('disabled', false)
+    })
+
+    // Pass click
+    $(pass).on('click', function() {
+        score.pass()
+        drawGuide(ctxGuide)
+    })
 
     // Board click
     $(scoreView).on('click', 'li', function() {
         moveFocus($(this))
     })
     $(input).on('mousedown', function(e) {
-        if (score.turn % 2 == 0 && blackPlayer == COM) return
-        if (score.turn % 2 != 0 && whitePlayer == COM) return
+        if (!playing) return
+        if (score.move == BLACK && blackPlayer == COM) return
+        if (score.move == WHITE && whitePlayer == COM) return
         var pos = adjustXY(e)
+        if (altMove.checked) {
+            if (score.turn == 1) {
+                if (pos.x < 6 || pos.x > 8 || pos.y < 6 || pos.y > 8) return
+            }
+            if (score.turn == 2) {
+                if (pos.x < 5 || pos.x > 9 || pos.y < 5 || pos.y > 9) return
+            }
+            if (score.turn == 4) {
+                if (alt5first == null) {
+                    alt5first = pos
+                    putStoneDirect(ctxStone, pos, BLACK)
+                    putNumber(ctxNumber, pos, "5A")
+                    drawGuide(ctxGuide)
+                    return
+                }
+                if (alt5second == null) {
+                    // Symmetry check
+                    alt5second = pos
+                    putStoneDirect(ctxStone, pos, BLACK)
+                    putNumber(ctxNumber, pos, "5B")
+                    drawGuide(ctxGuide)
+                    return
+                }
+                if (pos.equals(alt5first)) {
+                    clearStone(ctxStone, alt5first)
+                    clearNumber(ctxNumber, alt5first)
+                    clearNumber(ctxNumber, alt5second)
+                    pos = alt5second
+                }
+                else if (pos.equals(alt5second)) {
+                    clearStone(ctxStone, alt5second)
+                    clearNumber(ctxNumber, alt5first)
+                    clearNumber(ctxNumber, alt5second)
+                    pos = alt5first
+                }
+                else {
+                    return
+                }
+            }
+        }
         var stone = score.getStone(pos)
         if (stone == BLACK || stone == WHITE) return
 
         // Update Internal Model
         score.put(pos)
+
+        if (score.turn == 5) {
+            $(pass).prop('disabled', false)
+        }
 
 
         // Update Score View
@@ -227,6 +341,8 @@ $(window).on('load', function() {
             }
 
         }
+        drawGuide(ctxGuide)
+/*
         if ((score.turn % 2 == 0 && blackPlayer == COM) ||
             (score.turn % 2 != 0 && whitePlayer == COM)) {
             var buf = []
@@ -242,8 +358,8 @@ $(window).on('load', function() {
                 ws.send(msgpack.toByteArray(msgpack.pack(buf)))
             }
         }
+*/
     })
-
     // Score View
     var lastLi
     $("#scorediv").on('keydown',function(e) {
@@ -277,7 +393,7 @@ $(window).on('load', function() {
         var rect = e.target.getBoundingClientRect()
         mouseX = e.clientX - rect.left
         mouseY = e.clientY - rect.top
-        var boardX = Math.floor((mouseX - BASE / 2) / BASE) 
+        var boardX = Math.floor((mouseX - BASE / 2) / BASE)
         var boardY = Math.floor((mouseY - BASE / 2) / BASE)
         if (boardX < 0)    boardX = 0
         if (boardX >= MAX) boardX = MAX - 1
@@ -332,32 +448,31 @@ $(window).on('load', function() {
             ctx.fillText(i + 1, (i + 1) * BASE - 6, BASE - BASE / 2)
         }
     }
-    function putStone(ctx, pos) {
+    function putStoneDirect(ctx, pos, stone) {
         ctx.beginPath()
-        var stone = score.getStone(pos)
         if (stone == BLACK) {
             ctx.fillStyle = ctx.createRadialGradient(
-                (pos.x + 1) * BASE - 6, 
-                (pos.y + 1) * BASE - 6, 
-                0, 
-                (pos.x + 1) * BASE, 
-                (pos.y + 1) * BASE, 
+                (pos.x + 1) * BASE - 6,
+                (pos.y + 1) * BASE - 6,
+                0,
+                (pos.x + 1) * BASE,
+                (pos.y + 1) * BASE,
                 STONE_SIZE)
-            ctx.fillStyle.addColorStop(0  , "#AAAAAA") 
-            ctx.fillStyle.addColorStop(0.3, "#666666") 
-            ctx.fillStyle.addColorStop(1  , "#222222") 
+            ctx.fillStyle.addColorStop(0  , "#AAAAAA")
+            ctx.fillStyle.addColorStop(0.3, "#666666")
+            ctx.fillStyle.addColorStop(1  , "#222222")
         }
         else if (stone == WHITE) {
             ctx.fillStyle = ctx.createRadialGradient(
-                (pos.x + 1) * BASE - 6, 
-                (pos.y + 1) * BASE - 6, 
-                0, 
-                (pos.x + 1) * BASE, 
-                (pos.y + 1) * BASE, 
+                (pos.x + 1) * BASE - 6,
+                (pos.y + 1) * BASE - 6,
+                0,
+                (pos.x + 1) * BASE,
+                (pos.y + 1) * BASE,
                 STONE_SIZE)
-            ctx.fillStyle.addColorStop(0  , "#FFFFFF") 
-            ctx.fillStyle.addColorStop(0.8, "#DDDDDD") 
-            ctx.fillStyle.addColorStop(1  , "#BBBBBB") 
+            ctx.fillStyle.addColorStop(0  , "#FFFFFF")
+            ctx.fillStyle.addColorStop(0.8, "#DDDDDD")
+            ctx.fillStyle.addColorStop(1  , "#BBBBBB")
         }
         else {
             ctx.clearRect(
@@ -367,13 +482,24 @@ $(window).on('load', function() {
                 (pos.y + 1) * BASE - BASE / 2)
         }
         ctx.arc(
-            (pos.x + 1) * BASE, 
-            (pos.y + 1) * BASE, 
-            STONE_SIZE, 
-            0, 
-            Math.PI * 2, 
+            (pos.x + 1) * BASE,
+            (pos.y + 1) * BASE,
+            STONE_SIZE,
+            0,
+            Math.PI * 2,
             false)
         ctx.fill()
+    }
+    function putStone(ctx, pos) {
+        var stone = score.getStone(pos)
+        putStoneDirect(ctx, pos, stone)
+    }
+    function clearStone(ctx, pos) {
+        ctx.clearRect(
+            (pos.x + 1) * BASE - BASE / 2,
+            (pos.y + 1) * BASE - BASE / 2,
+            (pos.x + 1) * BASE + BASE / 2,
+            (pos.y + 1) * BASE - BASE / 2)
     }
     function setFocus(ctx, pos) {
         ctx.beginPath()
@@ -382,11 +508,11 @@ $(window).on('load', function() {
         ctx.strokeStyle = "#0066FF"
         var size = BASE / 2
         ctx.arc(
-            (pos.x + 1) * BASE, 
-            (pos.y + 1) * BASE, 
-            size, 
-            0, 
-            Math.PI * 2, 
+            (pos.x + 1) * BASE,
+            (pos.y + 1) * BASE,
+            size,
+            0,
+            Math.PI * 2,
             false)
         ctx.stroke()
     }
@@ -396,6 +522,13 @@ $(window).on('load', function() {
         ctx.fillStyle = "#33CC33"
         ctx.font = "16px 'Times New Roman'"
         ctx.fillText(no, (pos.x + 1) * BASE, (pos.y + 1) * BASE + 6)
+    }
+    function clearNumber(ctx, pos) {
+        ctx.clearRect(
+            (pos.x + 1) * BASE - BASE / 2,
+            (pos.y + 1) * BASE - BASE / 2,
+            (pos.x + 1) * BASE + BASE / 2,
+            (pos.y + 1) * BASE - BASE / 2)
     }
     function putYaku(ctx, positions) {
         ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
@@ -409,6 +542,92 @@ $(window).on('load', function() {
                 ctx.stroke()
             })
         }
+    }
+
+    function draw2ndGuide(ctx) {
+        ctx.beginPath()
+        ctx.textAlign = "center"
+        ctx.fillStyle = "#0066FF"
+        ctx.font = "16px 'Times New Roman'"
+        ctx.fillText("Put WHITE stone in the rectangle", 8 * BASE, 15 * BASE + BASE / 2 + 6)
+        ctx.lineWidth = 3
+        ctx.strokeStyle = "#0066FF"
+        var margin = BASE*0.3
+        ctx.rect((8-2)*BASE+margin, (8-2)*BASE+margin, BASE*4-margin*2, BASE*4-margin*2)
+        ctx.stroke()
+    }
+    function draw3rdGuide(ctx) {
+        ctx.beginPath()
+        ctx.textAlign = "center"
+        ctx.fillStyle = "#0066FF"
+        ctx.font = "16px 'Times New Roman'"
+        ctx.fillText("Put BLACK stone in the rectangle", 8 * BASE, 15 * BASE + BASE / 2 + 6)
+        ctx.lineWidth = 3
+        ctx.strokeStyle = "#0066FF"
+        var margin = BASE*0.3
+        ctx.rect((8-3)*BASE+margin, (8-3)*BASE+margin, BASE*6-margin*2, BASE*6-margin*2)
+        ctx.stroke()
+    }
+    function draw5_1Guide(ctx) {
+        ctx.beginPath()
+        ctx.textAlign = "center"
+        ctx.fillStyle = "#0066FF"
+        ctx.font = "16px 'Times New Roman'"
+        ctx.fillText("Put 1st candidate BLACK stone", 8 * BASE, 15 * BASE + BASE / 2 + 6)
+    }
+    function draw5_2Guide(ctx) {
+        ctx.beginPath()
+        ctx.textAlign = "center"
+        ctx.fillStyle = "#0066FF"
+        ctx.font = "16px 'Times New Roman'"
+        ctx.fillText("Put 2nd candidate BLACK stone", 8 * BASE, 15 * BASE + BASE / 2 + 6)
+    }
+    function draw5removeGuide(ctx) {
+        ctx.beginPath()
+        ctx.textAlign = "center"
+        ctx.fillStyle = "#0066FF"
+        ctx.font = "16px 'Times New Roman'"
+        ctx.fillText("Choose removing BLACK stone", 8 * BASE, 15 * BASE + BASE / 2 + 6)
+    }
+    function drawNormalGuide(ctx) {
+        ctx.beginPath()
+        ctx.textAlign = "center"
+        ctx.fillStyle = "#0066FF"
+        ctx.font = "16px 'Times New Roman'"
+        var stoneStr = score.move == BLACK ? "BLACK" : "WHITE"
+        ctx.fillText("Put " + stoneStr + " stone", 8 * BASE, 15 * BASE + BASE / 2 + 6)
+    }
+    function drawGuide(ctx) {
+        ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+        switch (score.turn) {
+        case 1:
+            if (altMove.checked) {
+                draw2ndGuide(ctx)
+                return
+            }
+            break
+        case 2:
+            if (altMove.checked) {
+                draw3rdGuide(ctx)
+                return
+            }
+            break
+        case 4:
+            if (altMove.checked) {
+                if (alt5first == null) {
+                    draw5_1Guide(ctx)
+                    return
+                }
+                if (alt5second == null) {
+                    draw5_2Guide(ctx)
+                    return
+                }
+                draw5removeGuide(ctx)
+                return
+            }
+            break
+        }
+        drawNormalGuide(ctx)
     }
 
     // Internal Functions for Model
@@ -475,7 +694,7 @@ $(window).on('load', function() {
                     r = { "yaku":Score.Y_WIN, "positions":result.positions }
                 }
             }
-        }) 
+        })
         return r
     }
     function isProhibited(pos) {
@@ -498,7 +717,7 @@ $(window).on('load', function() {
                     r = { "yaku":Score.Y_6, "positions":result.positions }
                 }
             }
-        }) 
+        })
         return r
     }
     function is4(pos) {
@@ -526,7 +745,7 @@ $(window).on('load', function() {
                     })
                 }
             }
-        }) 
+        })
         if (y44detected || count >= 2) {
             return { "yaku":Score.Y_44, "positions":positions }
         }
@@ -551,7 +770,7 @@ $(window).on('load', function() {
                     positions[v] = v
                 })
             }
-        }) 
+        })
         if (count >= 2) {
             return { "yaku":Score.Y_33, "positions":positions }
         }
@@ -617,7 +836,7 @@ $(window).on('load', function() {
         if (stone != BLACK && stone != WHITE) return null
         var enemy = stone == BLACK ? WHITE : BLACK
         var positions5 = []
-        var result = {} 
+        var result = {}
         $.each([Iterator.prototype.inc, Iterator.prototype.dec], function(i, mover) {
             var it = score.makeIterator(pos, dir)
             while (true) {
